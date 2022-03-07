b340a6e0db266f962da6996e2395fec711471758
toineto <augusto.vilhena4@gmail.com>
2022-03-07 18:32:28 -0300

    requisitos do 1 ao 5

---
+++ src/pages/Login.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+function Login({ history }) {
+  const [email, setEmail] = useState('');
+  const [button, setButton] = useState(true);
+  const [password, setPassword] = useState('');
+
+  function validateInputs() {
+    const regexEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    const MIN_PASSWORD = 6;
+    if (email.match(regexEmail) && parseFloat(password.length) > MIN_PASSWORD) {
+      return setButton(false);
+    }
+    return setButton(true);
+  }
+  useEffect(() => {
+    validateInputs();
+  }, [password, email]); // verificar depois
+
+  function handleButton(event) {
+    event.preventDefault();
+    console.log('ok');
+    // const data = await requestToken();
+    // this.setState({ token: data.token }, () => {
+    //   callback(this.state);
+    // });
+    // localStorage.setItem('token', data.token);
+    // const { history } = props;
+    history.push('/explore');
+  }
+
+  return (
+    <div className="container">
+      <form>
+        <label htmlFor="email">
+          Email
+          <input
+            id="email"
+            name="email"
+            type="email"
+            value={ email }
+            onChange={ ({ target }) => {
+              setEmail(target.value);
+              // validateInputs();
+            } }
+            data-testid="email-input"
+            placeholder="Email"
+          />
+        </label>
+        Senha
+        <label htmlFor="password">
+          <input
+            id="password"
+            name="password"
+            type="password"
+            value={ password }
+            onChange={ ({ target }) => {
+              setPassword(target.value);
+              // validateInputs();
+            } }
+            placeholder="Senha"
+            data-testid="password-input"
+          />
+        </label>
+        <button
+          disabled={ button }
+          data-testid="login-submit-btn"
+          onClick={ handleButton }
+          type="submit"
+        >
+          Enter
+        </button>
+      </form>
+    </div>
+  );
+}
+
+Login.propTypes = {
+  push: PropTypes.func,
+}.isRequired;
+
+export default Login;
