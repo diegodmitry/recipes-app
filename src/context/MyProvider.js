a6b03743417502aa4f5048de4672491610fd9eba
MatheusMena <matheusmena1@hotmail.com>
2022-03-09 23:03:07 -0300

    requisito 14

--- src/context/MyProvider.js
+++ src/context/MyProvider.js
@@ -1,40 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import MyContext from './MyContext';
 
 function MyProvider({ children }) {
   const [inputValue, setInputValue] = useState('');
+  const [currentFilter, setCurrentFilter] = useState('');
   const [ingredients, setIngredients] = useState([]);
   const [loading, setLoading] = useState(false);
-
-  const ApiIngredients = async (item) => {
-    const linkIngredient = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${item}`;
-    try {
-      const response = await fetch(linkIngredient);
-      const data = await response.json();
-      return data.meals;
-    } catch (error) {
-      return error;
-    }
-  };
-  useEffect(() => {
-    const handleChangeIngredient = async () => {
-      const returnIngredients = await ApiIngredients(inputValue);
-      setIngredients(returnIngredients);
-    };
-    handleChangeIngredient();
-  }, [inputValue]);
 
   console.log(ingredients);
 
   const value = {
-    handleChangeIngredient,
     setIngredients,
     ingredients,
     setInputValue,
     inputValue,
     loading,
     setLoading,
+    currentFilter,
+    setCurrentFilter,
   };
 
   return (
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -4,16 +4,19 @@ import Footer from '../components/Footer';
 import MyContext from '../context/MyContext';
 
 function Foods() {
-  const { ingredients, loading } = useContext(MyContext);
+  const { ingredients } = useContext(MyContext);
+  // const ingredientSearch = ingredients.filter((item) => item.strMeal.toUpperCase()
+  //   .includes(inputValue.toUpperCase()));
   return (
     <section>
       <Header />
       <h1>Main Screen Foods</h1>
-      { loading ? (ingredients.map((food) => (
-        <div className="card" key={ food.idMeal }>
-          <img src={ food.strMealThumb } alt="ImageCard" width="200px" height="200px" />
-          <h4><b>{food.strMeal}</b></h4>
-        </div>))) : '' }
+      {ingredients
+        .map((food) => (
+          <div className="card" key={ food.idMeal }>
+            <img src={ food.strMealThumb } alt="ImageCard" width="200px" height="200px" />
+            <h4><b>{food.strMeal}</b></h4>
+          </div>)) }
       <Footer />
     </section>
   );
