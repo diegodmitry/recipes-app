d82e817306a862d06ce6323212bcae1aca11dd4e
arthursantinati <arthursantinati02@outlook.com>
2022-03-11 09:28:35 -0300

    Building RecipeDetails Page

--- src/pages/RecipeDetailsDrinks.js
+++ src/pages/RecipeDetailsDrinks.js
@@ -5,43 +5,135 @@ function RecipeDetailsDrinks() {
   const { ingredients } = useContext(MyContext);
   return (
     <section>
-      <h1>Recipe details Foods</h1>
-      {ingredients.map((drinks) => (
+      <h1>Recipe details Drinks</h1>
+      {ingredients.map((foods, index) => (
         <div
           className="card"
-          key={ drinks.idMeal }
+          key={ foods.idDrink }
         >
-          <h4>
-            {drinks.strMeal}
+          <h4 data-testid="recipe-title">
+            {foods.strDrink}
           </h4>
-          <p>{ drinks.strDrinkAlternate }</p>
+          <button type="button" data-testid="share-btn">Compartilhar</button>
+          <button type="button" ddata-testid="favorite-btn">Compartilhar</button>
+          <p data-testid="recipe-category">{ foods.strCategory }</p>
           <img
-            src={ drinks.strMealThumb }
+            src={ foods.strDrinkThumb }
             alt="ImageCard"
             width="200px"
             height="200px"
+            data-testid="recipe-photo"
           />
-          <p>{ drinks.strInstructions }</p>
-          <p>{ drinks.strIngredient1 }</p>
-          <p>{ drinks.strIngredient2 }</p>
-          <p>{ drinks.strIngredient3 }</p>
-          <p>{ drinks.strIngredient4 }</p>
-          <p>{ drinks.strIngredient5 }</p>
-          <p>{ drinks.strIngredient6 }</p>
-          <p>{ drinks.strIngredient7 }</p>
-          <p>{ drinks.strIngredient8 }</p>
-          <p>{ drinks.strIngredient9 }</p>
-          <p>{ drinks.strIngredient10 }</p>
-          <p>{ drinks.strIngredient11 }</p>
-          <p>{ drinks.strIngredient12 }</p>
-          <p>{ drinks.strIngredient13 }</p>
-          <p>{ drinks.strIngredient14 }</p>
-          <p>{ drinks.strIngredient15 }</p>
-          <p>{ drinks.strIngredient16 }</p>
-          <p>{ drinks.strIngredient17 }</p>
-          <p>{ drinks.strIngredient18 }</p>
-          <p>{ drinks.strIngredient19 }</p>
-          <p>{ drinks.strIngredient20 }</p>
+          <p data-testid="instructions">
+            { foods.strInstructions }
+          </p>
+          <iframe title="video" data-testid="video" src="">VIdeo</iframe>
+          <p
+            data-testid={ `${index}-recomendation-card` }
+          >
+            Receitas recomendadas
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient1 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient2 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient3 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient4 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient5 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient6 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient7 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient8 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient9 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient10 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient11 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient12 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient13 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient14 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient15 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient16 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient17 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient18 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient19 }
+          </p>
+          <p
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { foods.strIngredient20 }
+          </p>
+          <button type="button" data-testid="start-recipe-btn">Start Recipe</button>
         </div>
       ))}
     </section>
