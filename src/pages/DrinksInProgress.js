bec41ffa086a79bb0851e8158defd8dac8f8fc09
arthursantinati <arthursantinati02@outlook.com>
2022-03-16 09:21:47 -0300

    alteraçoes que nao devem mergeadas

--- src/pages/DrinksInProgress.js
+++ src/pages/DrinksInProgress.js
@@ -1,17 +1,180 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+// import { ApiFoodById } from '../services/ApiMeals';
+import { ApiDrinkById } from '../services/ApiDrinks';
+import shareIcon from '../images/shareIcon.svg';
+import MyContext from '../context/MyContext';
 
 export default function DrinksInProgress() {
   const history = useHistory();
+  const { id } = useParams();
+  const [drinkDetail, setDrinkDetail] = useState([]);
+  const { btnLike } = useContext(MyContext);
+
+  useEffect(() => {
+    async function getId() {
+      const result = await ApiDrinkById(id);
+      return setDrinkDetail(result);
+    }
+    getId();
+  }, [id]);
+
   return (
     <div>
-      DrinksInProgress
+      Drinks In Progres
+      {drinkDetail.map((item) => (
+        <div
+          className="card"
+          key={ item.idDrink }
+        >
+          <img src={ item.strDrinkThumn } alt="ImageCard" data-testid="recipe-photo" />
+          <h4 data-testid="recipe-title">
+            {item.strDrink}
+          </h4>
+          {btnLike()}
+          <button
+            type="button"
+            data-testid="share-btn"
+            onClick={ console.log('share btn') }
+          >
+            <img
+              alt="share_icon"
+              src={ shareIcon }
+            />
+          </button>
+          <p data-testid="recipe-category">{ item.strCategory }</p>
+          <div>
+            <h4>Instructions</h4>
+            <p data-testid="instructions">{ item.strInstructions }</p>
+            <p
+              data-testid="1-ingredient-step"
+            >
+              {item.strMeasure}
+              {item.strIngredient1}
+            </p>
+            <p
+              data-testid="2-ingredient-step"
+            >
+              {item.strIngredient2}
+              {item.strMeasure2}
+            </p>
+            <p
+              data-testid="3-ingredient-step"
+            >
+              {item.strIngredient3}
+              {item.strMeasure3}
+            </p>
+            <p
+              data-testid="4-ingredient-step"
+            >
+              {item.strIngredient4}
+              {item.strMeasure4}
+            </p>
+            <p
+              data-testid="5-ingredient-step"
+            >
+              {item.strIngredient5}
+              {item.strMeasure5}
+            </p>
+            <p
+              data-testid="6-ingredient-step"
+            >
+              {item.strIngredient6}
+              {item.strMeasure6}
+            </p>
+            <p
+              data-testid="7-ingredient-step"
+            >
+              {item.strIngredient7}
+              {item.strMeasure7}
+            </p>
+            <p
+              data-testid="8-ingredient-step"
+            >
+              {item.strIngredient8}
+              {item.strMeasure8}
+            </p>
+            <p
+              data-testid="9-ingredient-step"
+            >
+              {item.strIngredient9}
+              {item.strMeasure9}
+            </p>
+            <p
+              data-testid="10-ingredient-step"
+            >
+              {item.strIngredient10}
+              {item.strMeasure10}
+            </p>
+            <p
+              data-testid="11-ingredient-step"
+            >
+              {item.strIngredient11}
+              {item.strMeasure11}
+            </p>
+            <p
+              data-testid="12-ingredient-step"
+            >
+              {item.strIngredient12}
+              {item.strMeasure12}
+            </p>
+            <p
+              data-testid="13-ingredient-step"
+            >
+              {item.strIngredient13}
+              {item.strMeasure13}
+            </p>
+            <p
+              data-testid="14-ingredient-step"
+            >
+              {item.strIngredient14}
+              {item.strMeasure14}
+            </p>
+            <p
+              data-testid="15-ingredient-step"
+            >
+              {item.strIngredient15}
+              {item.strMeasure15}
+            </p>
+            <p
+              data-testid="16-ingredient-step"
+            >
+              {item.strIngredient16}
+              {item.strMeasure16}
+            </p>
+            <p
+              data-testid="17-ingredient-step"
+            >
+              {item.strIngredient17}
+              {item.strMeasure17}
+            </p>
+            <p
+              data-testid="18-ingredient-step"
+            >
+              {item.strIngredient18}
+              {item.strMeasure18}
+            </p>
+            <p
+              data-testid="19-ingredient-step"
+            >
+              {item.strIngredient19}
+              {item.strMeasure19}
+            </p>
+            <p
+              data-testid="20-ingredient-step"
+            >
+              {item.strIngredient20}
+              {item.strMeasure20}
+            </p>
+          </div>
+        </div>
+      ))}
       <button
         type="button"
+        data-testid="finish-recipe-btn"
         onClick={ () => history.push('/done-recipes') }
       >
         Finish Recipe
-
       </button>
     </div>
   );
--- src/pages/FoodsInProgress.js
+++ src/pages/FoodsInProgress.js
@@ -1,15 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import { ApiFoodById } from '../services/ApiMeals';
+import shareIcon from '../images/shareIcon.svg';
+import MyContext from '../context/MyContext';
 
 export default function FoodsInProgress() {
   const history = useHistory();
   const { id } = useParams();
   const [foodDetail, setFoodDetail] = useState([]);
+  const [paragraph, setParagraph] = useState([]);
+  const { btnLike } = useContext(MyContext);
+
+  function renderParagraph(result) {
+    // if (result[0] !== null || result[0] !== undefined) {
+    // const keys = Object.entries(result[0]);
+    // const values = Object.values(result[0]);
+    // const filteredKeys = keys.filter((item) => (
+    //   // item.match(/strIngredient/i) || item.match(/strMeasure/i)
+    //   item[0].match(/strIngredient/i) || item[0].match(/strMeasure/i)
+    // ));
+    // setParagraph(filteredKeys);
+    const ingredients = Object.fromEntries(
+      Object.entries(result[0]).filter(([key]) => key.match(/strIngredient/i)),
+    );
+    const measure = Object.fromEntries(
+      Object.entries(result[0]).filter(([key]) => key.match(/strMeasure/i)),
+    );
+    const objetoGeral = { ...ingredients, ...measure };
+    console.log('obj', objetoGeral);
+    setParagraph([objetoGeral]);
+    // }
+  }
+  // console.log(paragraph);
 
   useEffect(() => {
     async function getId() {
       const result = await ApiFoodById(id);
+      renderParagraph(result);
       return setFoodDetail(result);
     }
     getId();
@@ -17,19 +44,44 @@ export default function FoodsInProgress() {
 
   return (
     <div>
-      FoodsInProgress
       {foodDetail.map((item) => (
         <div
           className="card"
           key={ item.idMeal }
         >
+          <img src={ item.strMealThumb } alt="ImageCard" data-testid="recipe-photo" />
           <h4 data-testid="recipe-title">
             {item.strMeal}
           </h4>
+          {btnLike()}
+          <button
+            type="button"
+            data-testid="share-btn"
+          >
+            <img
+              alt="share_icon"
+              src={ shareIcon }
+            />
+          </button>
+          <p data-testid="recipe-category">{ item.strCategory }</p>
+          <div>
+            <h4>Instructions</h4>
+            <p data-testid="instructions">{ item.strInstructions }</p>
+            {/* <p
+              data-testid={ `${index}-ingredient-step` }
+            >
+              {`${item}.strMeasure1`}
+              {`${item}.strIngredintet`}
+            </p> */}
+            { paragraph.map((a) => (
+              <p key={ a.strIngredient20 }>{a.strIngredient1}</p>
+            ))}
+          </div>
         </div>
       ))}
       <button
         type="button"
+        data-testid="finish-recipe-btn"
         onClick={ () => history.push('/done-recipes') }
       >
         Finish Recipe
